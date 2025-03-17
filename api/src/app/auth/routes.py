6b3c1d2b7651d8b09e9fa289c96cdb5c218c219d
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-03-17 16:40:47 -0400

    Move token generation to separate method

--- api/src/app/auth/routes.py
+++ api/src/app/auth/routes.py
@@ -1,10 +1,7 @@
-import datetime
-
 import bcrypt
-import jwt
 from fastapi import APIRouter
 
-from app.auth import schemas
+from app.auth import functions, schemas
 from app.configs import configs
 from app.database import DBSessionDependency
 from app.helpers.decorators import public
@@ -25,15 +22,7 @@ async def login(user_details: schemas.UserInput, db_session: DBSessionDependency
         password.encode("utf-8"), configs.PASSWORD_HASH.encode("utf-8")
     )
     if password_check:
-        # jwt = exp_len = {"weeks": 2}
         return {
-            "jwt": jwt.encode(
-                {
-                    "exp": datetime.datetime.now(datetime.timezone.utc)
-                    + datetime.timedelta(weeks=2),
-                },
-                key=configs.JWT_SECRET_KEY,
-                algorithm=configs.JWT_ALGORITHM,
-            )
+            "jwt": functions.generate_token(),
         }
     return error_response(status_code=422, content=[{"error": "invalid"}])
