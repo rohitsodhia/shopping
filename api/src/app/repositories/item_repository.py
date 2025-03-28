76a75016fa93f3d7ab569666605d9c42302295da
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-03-28 17:38:06 -0400

    Create item repository tests

--- api/src/app/repositories/item_repository.py
+++ api/src/app/repositories/item_repository.py
@@ -22,9 +22,8 @@ class ItemRepository:
         try:
             await self.db_session.commit()
         except SQLAIntegrityError as e:
-            if str(e.orig):
-                if insert_vals := parse_integrity_error(str(e.orig)):
-                    raise IntegrityError(*insert_vals)
+            await self.db_session.rollback()
+            raise AlreadyExists(e)
         return item
 
     async def count(self, name_like: str | None = None) -> int:
@@ -35,24 +34,17 @@ class ItemRepository:
 
     async def get_all(
         self,
-        *,
-        fields: list[str] | None = None,
         page: int = 1,
+        *,
         name_like: str | None = None,
-        ids: list[int] | None = None,
     ) -> Sequence[Item]:
-        if fields:
-            statement = select(*[getattr(Item, f) for f in fields])
-        else:
-            statement = select(Item)
+        statement = select(Item)
 
         statement = statement.limit(configs.PAGINATE_PER_PAGE).offset(
             (page - 1) * configs.PAGINATE_PER_PAGE
         )
         if name_like:
             statement = statement.where(Item.name.like(f"%{name_like}%"))
-        if ids:
-            statement = statement.where(Item.id.in_(ids))
 
         items = await self.db_session.scalars(statement)
         return items.all()
@@ -71,11 +63,8 @@ class ItemRepository:
         if notes is not None:
             item.notes = notes
 
-        db_check = await self.db_session.scalar(
-            select(Item).where(func.lower(Item.name) == item.name.lower()).limit(1)
-        )
-        if db_check and db_check.id != item.id:
-            raise AlreadyExists(db_check)
-
-        await self.db_session.commit()
+        try:
+            await self.db_session.commit()
+        except SQLAIntegrityError as e:
+            raise AlreadyExists(e)
         return item
