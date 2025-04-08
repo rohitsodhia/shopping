f3de2e34cde843a3740b9bae92c7bebd24bfc47f
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-04-08 19:44:57 -0400

    Create login form and processor

--- src/app/auth/routes.py
+++ src/app/auth/routes.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from typing import Annotated
 
-from app.configs import templates
+import bcrypt
+from fastapi import APIRouter, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from app.auth import functions
+from app.configs import configs, templates
 from app.helpers.decorators import public
 
 auth = APIRouter(prefix="/login")
@@ -9,5 +13,19 @@ auth = APIRouter(prefix="/login")
 
 @auth.get("", response_class=HTMLResponse)
 @public
-async def read_item(request: Request):
+async def login_page(request: Request):
     return templates.TemplateResponse(request=request, name="login.html", context={})
+
+
+@auth.post("", response_class=RedirectResponse)
+@public
+async def process_login(request: Request, password: Annotated[str, Form()]):
+    password_check = bcrypt.checkpw(
+        password.encode("utf-8"), configs.PASSWORD_HASH.encode("utf-8")
+    )
+    if not password_check:
+        return RedirectResponse(url="/login?incorrect=1", status_code=302)
+
+    response = RedirectResponse(url="/", status_code=302)
+    response.set_cookie(key="auth", value=functions.generate_token(), httponly=True)
+    return response
