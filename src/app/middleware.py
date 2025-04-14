022fab4415afe01d0858f57479aad70a2fb409be
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-04-14 18:05:57 -0400

    Do auth via cookies always

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -1,18 +1,11 @@
 import jwt
 from fastapi import HTTPException, Request, status
-from fastapi.responses import RedirectResponse
 
 from app.configs import configs
 
 
 async def validate_jwt(request: Request):
-    path = request.scope["route"].path
-    if path.startswith("/api/"):
-        token = request.headers.get("Authorization")
-        if token:
-            token = token[7:]
-    else:
-        token = request.cookies.get("auth")
+    token = request.cookies.get("auth")
     request.scope["auth"] = None
     request.scope["user"] = None
     if token:
@@ -24,7 +17,9 @@ async def validate_jwt(request: Request):
             )
             request.scope["user"] = "valid"
         except:
-            pass
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden"
+            )
 
 
 async def check_authorization(request: Request):
