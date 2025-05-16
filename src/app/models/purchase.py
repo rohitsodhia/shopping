063e8e69c11cd5cacfb1feae3c7a8dcdeae4b95d
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-05-16 13:58:34 -0400

    Create amount db field

--- src/app/models/purchase.py
+++ src/app/models/purchase.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, Integer, Text
+from sqlalchemy import ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
@@ -20,4 +20,5 @@ class Purchase(Base):
     receipt_id: Mapped[int] = mapped_column(ForeignKey("receipts.id"))
     receipt: Mapped["Receipt"] = relationship(back_populates="purchases")
     price: Mapped[int] = mapped_column(Integer(), nullable=True)
+    amount: Mapped[str | None] = mapped_column(String(), nullable=True)
     notes: Mapped[str | None] = mapped_column(Text(), nullable=True)
